9530b0b6bfacacdc00de6b4e328644d40e9bacbb
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-07-15 12:58:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ droppableElements.forEach(elem => {
    elem.addEventListener("drop", drop);
 })
 
-const planets = document.querySelectorAll('.planet')
+const planets = document.querySelectorAll('.droppable')
 const p_radii = [127,112,97,84,60,50,35,25]
 
 const p_orbits = document.querySelectorAll('.orbit')
@@ -26,28 +26,16 @@ p_orbits.forEach((p_orbit, index)=>{
 })
 
 planets.forEach((planet, index) =>{
-   if(index == 4 || index == 6){
+   if(index == 4 || index == 6 || index == 2 || index == 0){
       planet.style.top= `${0.5*p_radii[index]}vmin`;
       planet.style.left = `${0.5*p_radii[index]}vmin`}
-   else if(index == 1){
+   else if(index == 1 || index == 3 || index == 5){
       planet.style.top = `${0.5*p_radii[index]}vmin`;
       planet.style.left = `${-0.5*p_radii[index]}vmin`
    }
    else if(index == 7){
       planet.style.top = `${0.5*p_radii[index]}vmin`;
       planet.style.left = `${-0.45*p_radii[index]}vmin`
-   }
-   else if(index == 2 || index == 0){
-      planet.style.top = `${0.5*p_radii[index]}vmin`;
-      planet.style.left = `${0.5*p_radii[index]}vmin`
-   }
-   else if(index == 3){
-      planet.style.top = `${0.5*p_radii[index]}vmin`;
-      planet.style.left = `${-0.5*p_radii[index]}vmin`
-   }
-   else if(index == 5){
-      planet.style.top = `${0.5*p_radii[index]}vmin`;
-      planet.style.left = `${-0.5*p_radii[index]}vmin`
    }
 })
 
