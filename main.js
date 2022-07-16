ff969933861624b88b90e168f90028dc9ae17930
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-07-16 13:19:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,28 +26,16 @@ p_orbits.forEach((p_orbit, index)=>{
 })
 
 planets.forEach((planet, index) =>{
-   if(index == 4 || index == 6){
+   if(index == 4 || index == 6 || index == 2 || index == 0){
       planet.style.top= `${0.5*p_radii[index]}vmin`;
       planet.style.left = `${0.5*p_radii[index]}vmin`}
-   else if(index == 1){
+   else if(index == 1 || index == 3 || index == 5){
       planet.style.top = `${0.5*p_radii[index]}vmin`;
       planet.style.left = `${-0.5*p_radii[index]}vmin`
    }
    else if(index == 7){
       planet.style.top = `${0.5*p_radii[index]}vmin`;
       planet.style.left = `${-0.45*p_radii[index]}vmin`
-   }
-   else if(index == 2 || index == 0){
-      planet.style.top = `${0.5*p_radii[index]}vmin`;
-      planet.style.left = `${0.5*p_radii[index]}vmin`
-   }
-   else if(index == 3){
-      planet.style.top = `${0.5*p_radii[index]}vmin`;
-      planet.style.left = `${-0.5*p_radii[index]}vmin`
-   }
-   else if(index == 5){
-      planet.style.top = `${0.5*p_radii[index]}vmin`;
-      planet.style.left = `${-0.5*p_radii[index]}vmin`
    }
 })
 
