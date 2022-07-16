810067f8ade07e298989ac52534b58428c5c8c6c
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-07-16 20:30:40 +0530

    Update guide.js

--- guide.js
+++ guide.js
@@ -4,11 +4,8 @@ function OKbutton(){
     if(currGuide==="jupiter"){
         if(document.getElementById("button").innerHTML==='OKAY'){
             var firstText = "Drag the largest planet in the solar system! It is a gas giant and its colour changes with storms and wind in the planet's atmosphere. Here, drag the planet yellowish-brown in colour!";
-            document.getElementById("instruct").innerHTML = '<div>'+ firstText + '</div>'            
-        }
-        
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Jupiter.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("instruct").innerHTML = '<div>'+ firstText + '</div>'    
+            document.getElementById("button").style.display = 'none';        
         }
         currGuide="venus";  
         
@@ -18,10 +15,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='NEXT'){
             var secondText = "Drag the yellowish-white planet with few brownish-red spots. These spots signify the volcanic rocks!";
             document.getElementById("instruct").innerHTML = '<div>'+ secondText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Venus.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide="mars";
     }
@@ -30,10 +24,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='LETS MOVE AHEAD!'){
             var thirdText = "Coming to the red planet! It got its colour red because of the rusty iron on its surface.";
             document.getElementById("instruct").innerHTML = '<div>'+ thirdText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Mars.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide="uranus"
     }
@@ -42,10 +33,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='NEXT PLANET'){
             var fourthText = "Drag the planet which is blue in colour and has faint rings.";
             document.getElementById("instruct").innerHTML = '<div>'+ fourthText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Uranus.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide="earth";
     }
@@ -54,10 +42,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='LETS GO!'){
             var fifthText = "Next is our home! A combination of water and land! It is a rocky, terrestrial planet. It has a solid and active surface with mountains, valleys, canyons and plains.";
             document.getElementById("instruct").innerHTML = '<div>'+ fifthText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Earth.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide="mercury";
     }
@@ -66,10 +51,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='THIS IS FUN!'){
             var sixthText = "Drag the dark grey planet that has wrinkles on it!";
             document.getElementById("instruct").innerHTML = '<div>'+ sixthText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Mercury.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide="neptune";
     }
@@ -78,10 +60,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='GO AHEAD!'){
             var seventhText = "The last one is a dark, cold and windy planet! ";
             document.getElementById("instruct").innerHTML = '<div>'+ seventhText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Neptune.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide="saturn";
     }
@@ -90,10 +69,7 @@ function OKbutton(){
         if(document.getElementById("button").innerHTML==='LAST PLANET'){
             var eighthText = "Drag the planet with rings! It has a set of 7 main rings with spaces between them.";
             document.getElementById("instruct").innerHTML = '<div>'+ eighthText + '</div>'
-        }
-    
-        if(document.getElementById("button").innerHTML==='YES'){
-            document.getElementById("instruct").innerHTML='<img src="closeup-images/Saturn.jpg" style="height:100%; width:100%;"></img>'
+            document.getElementById("button").style.display = 'none';
         }
         currGuide=null;
     }
