6dd7d23f627d23b713a0fcf2894f12f194cd8c98
Deepti-tech <80256433+Deepti-tech@users.noreply.github.com>
2022-07-17 21:39:05 +0530

    Update guide.js

--- javascripts/guide.js
+++ javascripts/guide.js
@@ -1,76 +1,29 @@
 function OKbutton(){
-    var currGuide="jupiter";
-
-    if(currGuide==="jupiter"){
-        if(document.getElementById("button").innerHTML==='OKAY'){
-            var firstText = "Drag the largest planet in the solar system! It is a gas giant and its colour changes with storms and wind in the planet's atmosphere. Here, drag the planet yellowish-brown in colour!";
-            document.getElementById("instruct").innerHTML = '<div>'+ firstText + '</div>'    
-            document.getElementById("button").style.display = 'none';        
-        }
-        currGuide="venus";  
-        
-    }
-
-    if(currGuide==="venus"){
-        if(document.getElementById("button").innerHTML==='NEXT'){
-            var secondText = "Drag the yellowish-white planet with few brownish-red spots. These spots signify the volcanic rocks!";
-            document.getElementById("instruct").innerHTML = '<div>'+ secondText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide="mars";
-    }
-
-    if(currGuide==="mars"){
-        if(document.getElementById("button").innerHTML==='LETS MOVE AHEAD!'){
-            var thirdText = "Coming to the red planet! It got its colour red because of the rusty iron on its surface.";
-            document.getElementById("instruct").innerHTML = '<div>'+ thirdText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide="uranus"
-    }
-
-    if(currGuide==="uranus"){
-        if(document.getElementById("button").innerHTML==='NEXT PLANET'){
-            var fourthText = "Drag the planet which is blue in colour and has faint rings.";
-            document.getElementById("instruct").innerHTML = '<div>'+ fourthText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide="earth";
-    }
-
-    if(currGuide==="earth"){
-        if(document.getElementById("button").innerHTML==='LETS GO!'){
-            var fifthText = "Next is our home! A combination of water and land! It is a rocky, terrestrial planet. It has a solid and active surface with mountains, valleys, canyons and plains.";
-            document.getElementById("instruct").innerHTML = '<div>'+ fifthText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide="mercury";
-    }
-
-    if(currGuide==="mercury"){
-        if(document.getElementById("button").innerHTML==='THIS IS FUN!'){
-            var sixthText = "Drag the dark grey planet that has wrinkles on it!";
-            document.getElementById("instruct").innerHTML = '<div>'+ sixthText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide="neptune";
-    }
-
-    if(currGuide==="neptune"){
-        if(document.getElementById("button").innerHTML==='GO AHEAD!'){
-            var seventhText = "The last one is a dark, cold and windy planet! ";
-            document.getElementById("instruct").innerHTML = '<div>'+ seventhText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide="saturn";
-    }
-
-    if(currGuide==="saturn"){
-        if(document.getElementById("button").innerHTML==='LAST PLANET'){
-            var eighthText = "Drag the planet with rings! It has a set of 7 main rings with spaces between them.";
-            document.getElementById("instruct").innerHTML = '<div>'+ eighthText + '</div>'
-            document.getElementById("button").style.display = 'none';
-        }
-        currGuide=null;
-    }
+    document.getElementById("button").style.display = 'none'; 
+    var Text;
+    if(document.getElementById("button").value==='1'){
+        Text = "Drag the dark grey planet that has wrinkles on it!";
+    }   
+    if(document.getElementById("button").value==='2'){
+        Text = "Drag the yellowish-white planet with few brownish-red spots. These spots signify the volcanic rocks!";
+    } 
+    if(document.getElementById("button").value==='3'){
+        Text = "Next is our home! A combination of water and land! It is a rocky, terrestrial planet. It has a solid and active surface with mountains, valleys, canyons and plains.";
+    } 
+    if(document.getElementById("button").value==='4'){
+        Text = "Coming to the red planet! It got its colour red because of the rusty iron on its surface.";
+    } 
+    if(document.getElementById("button").value==='5'){
+        Text = "Drag the largest planet in the solar system! It is a gas giant and its colour changes with storms and wind in the planet's atmosphere. Here, drag the planet yellowish-brown in colour!";           
+    } 
+    if(document.getElementById("button").value==='6'){
+        Text = "Drag the planet with rings! It has a set of 7 main rings with spaces between them.";
+    } 
+    if(document.getElementById("button").value==='7'){
+        Text = "Drag the planet which is blue in colour and has faint rings.";
+    } 
+    if(document.getElementById("button").value==='8'){
+        Text = "The last one is a dark, cold and windy planet! ";
+    } 
+    document.getElementById("instruct").innerHTML = Text
 }
