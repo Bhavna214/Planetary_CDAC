7a19b0bc298a267df30e0be664350625262fb908
Bhavna214 <bhavnamulchandani214@gmail.com>
2023-03-04 15:22:45 +0530

    multilingual

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,134 @@ var i = 0;
 var error = []
 var myplanets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
 
+const countries = {
+   "am-ET": "Amharic",
+   "ar-SA": "Arabic",
+   "be-BY": "Bielarus",
+   "bem-ZM": "Bemba",
+   "bi-VU": "Bislama",
+   "bjs-BB": "Bajan",
+   "bn-IN": "Bengali",
+   "bo-CN": "Tibetan",
+   "br-FR": "Breton",
+   "bs-BA": "Bosnian",
+   "ca-ES": "Catalan",
+   "cop-EG": "Coptic",
+   "cs-CZ": "Czech",
+   "cy-GB": "Welsh",
+   "da-DK": "Danish",
+   "dz-BT": "Dzongkha",
+   "de-DE": "German",
+   "dv-MV": "Maldivian",
+   "el-GR": "Greek",
+   "en-GB": "English",
+   "es-ES": "Spanish",
+   "et-EE": "Estonian",
+   "eu-ES": "Basque",
+   "fa-IR": "Persian",
+   "fi-FI": "Finnish",
+   "fn-FNG": "Fanagalo",
+   "fo-FO": "Faroese",
+   "fr-FR": "French",
+   "gl-ES": "Galician",
+   "gu-IN": "Gujarati",
+   "ha-NE": "Hausa",
+   "he-IL": "Hebrew",
+   "hi-IN": "Hindi",
+   "hr-HR": "Croatian",
+   "hu-HU": "Hungarian",
+   "id-ID": "Indonesian",
+   "is-IS": "Icelandic",
+   "it-IT": "Italian",
+   "ja-JP": "Japanese",
+   "kk-KZ": "Kazakh",
+   "km-KM": "Khmer",
+   "kn-IN": "Kannada",
+   "ko-KR": "Korean",
+   "ku-TR": "Kurdish",
+   "ky-KG": "Kyrgyz",
+   "la-VA": "Latin",
+   "lo-LA": "Lao",
+   "lv-LV": "Latvian",
+   "men-SL": "Mende",
+   "mg-MG": "Malagasy",
+   "mi-NZ": "Maori",
+   "ms-MY": "Malay",
+   "mt-MT": "Maltese",
+   "my-MM": "Burmese",
+   "ne-NP": "Nepali",
+   "niu-NU": "Niuean",
+   "nl-NL": "Dutch",
+   "no-NO": "Norwegian",
+   "ny-MW": "Nyanja",
+   "ur-PK": "Pakistani",
+   "pau-PW": "Palauan",
+   "pa-IN": "Panjabi",
+   "ps-PK": "Pashto",
+   "pis-SB": "Pijin",
+   "pl-PL": "Polish",
+   "pt-PT": "Portuguese",
+   "rn-BI": "Kirundi",
+   "ro-RO": "Romanian",
+   "ru-RU": "Russian",
+   "sg-CF": "Sango",
+   "si-LK": "Sinhala",
+   "sk-SK": "Slovak",
+   "sm-WS": "Samoan",
+   "sn-ZW": "Shona",
+   "so-SO": "Somali",
+   "sq-AL": "Albanian",
+   "sr-RS": "Serbian",
+   "sv-SE": "Swedish",
+   "sw-SZ": "Swahili",
+   "ta-LK": "Tamil",
+   "te-IN": "Telugu",
+   "tet-TL": "Tetum",
+   "tg-TJ": "Tajik",
+   "th-TH": "Thai",
+   "ti-TI": "Tigrinya",
+   "tk-TM": "Turkmen",
+   "tl-PH": "Tagalog",
+   "tn-BW": "Tswana",
+   "to-TO": "Tongan",
+   "tr-TR": "Turkish",
+   "uk-UA": "Ukrainian",
+   "uz-UZ": "Uzbek",
+   "vi-VN": "Vietnamese",
+   "wo-SN": "Wolof",
+   "xh-ZA": "Xhosa",
+   "yi-YD": "Yiddish",
+   "zu-ZA": "Zulu"
+}
+
+// const formText=document.querySelector("#instruct");
+// const toText=document.querySelector(".to-text");
+// selectTag=document.querySelector("select");
+// translatorBtn=document.querySelector(".translate");
+var translateFrom="en";
+var translateTo=selectTag.value;
+
+function translateLang(){
+   const toText=document.querySelector(".to-text");
+
+   let text=document.getElementById("instruct").innerText
+   if(!text)return;
+   console.log(text)
+   // toText.setAttribute("placeholder","Translating..");
+
+   // text = OKbutton();
+
+   let api_url="https://api.mymemory.translated.net/get?q="+text+"&langpair="+translateFrom+"|"+translateTo;
+   console.log(api_url)
+   fetch(api_url).then(res=>res.json()).then(data=>{
+   // console.log(data);
+   // document.getElementById("instruct").innerText=data.responseData.translatedText;
+   translatedText = data.responseData.translatedText;
+   toText.setAttribute("placeholder","Translation");
+   dexter_voice(translateTo, translatedText)
+   });
+}
+
 while(arr.length < 8){
    var r = Math.floor(Math.random() * 8) + 1;
    if(arr.indexOf(r) === -1) arr.push(r);
@@ -178,8 +306,8 @@ function dragStart(event) {
    }
    document.getElementById("instruct").innerHTML = Pos;
    currPlanet=nextPlanet;
-
-   dexter_voice();
+   //dexter_voice();
+   translateLang();
 }
 
 function removeDuplicates(arr) {
@@ -304,7 +432,9 @@ function drop(event) {
       else{
          document.getElementById("instruct").innerHTML= p+" lies between " + p1 +" and " + p2;
       }
-      dexter_voice();
+      
+      //dexter_voice();
+      translateLang();
    }
    displayFacts();
    
